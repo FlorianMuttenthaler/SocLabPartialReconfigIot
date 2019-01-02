4cc9cf8c73925213f703f1ccc4c46747ff4b795a
FlorianMuttenthaler <flo.muttenthaler@gmail.com>
2019-01-02 13:47:39 +0100

    added Color Correction IP design in ZYNQ block design environment

--- ImageProcessing/gammacorrection/gammacorrection.srcs/sources_1/bd/zynq_design/sim/zynq_design.v
+++ ImageProcessing/gammacorrection/gammacorrection.srcs/sources_1/bd/zynq_design/sim/zynq_design.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
-//Date        : Wed Jan  2 13:31:48 2019
+//Date        : Wed Jan  2 13:34:37 2019
 //Host        : DESKTOP-8PEEVNN running 64-bit major release  (build 9200)
 //Command     : generate_target zynq_design.bd
 //Design      : zynq_design
