53c8ce1bc06caa238fab4abdad83199b8a5ce9c9
FlorianMuttenthaler <flo.muttenthaler@gmail.com>
2019-01-06 13:57:30 +0100

    Image processing: added c file as streaming interface

--- ImageProcessing/gammacorrection/gammacorrection.srcs/sources_1/bd/zynq_design/hdl/zynq_design_wrapper.vhd
+++ ImageProcessing/gammacorrection/gammacorrection.srcs/sources_1/bd/zynq_design/hdl/zynq_design_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
---Date        : Wed Jan  2 14:16:52 2019
+--Date        : Wed Jan  2 14:20:21 2019
 --Host        : DESKTOP-8PEEVNN running 64-bit major release  (build 9200)
 --Command     : generate_target zynq_design_wrapper.bd
 --Design      : zynq_design_wrapper
