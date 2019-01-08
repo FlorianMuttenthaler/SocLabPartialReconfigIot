09c857de2e413b20d1bbe71deac719dc4b9fc2f8
FlorianMuttenthaler <flo.muttenthaler@gmail.com>
2019-01-08 11:57:03 +0100

    Image processing projects with vivado version of commom virtual machine

--- ImageProcessing/gammacorrection/gammacorrection.srcs/sources_1/bd/zynq_design/hdl/zynq_design_wrapper.vhd
+++ ImageProcessing/gammacorrection/gammacorrection.srcs/sources_1/bd/zynq_design/hdl/zynq_design_wrapper.vhd
@@ -1,8 +1,8 @@
---Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+--Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
---Date        : Wed Jan  2 14:20:21 2019
---Host        : DESKTOP-8PEEVNN running 64-bit major release  (build 9200)
+--Tool Version: Vivado v.2017.4.1 (lin64) Build 2117270 Tue Jan 30 15:31:13 MST 2018
+--Date        : Tue Jan  8 11:41:48 2019
+--Host        : soclab-VirtualBox running 64-bit Ubuntu 18.04.1 LTS
 --Command     : generate_target zynq_design_wrapper.bd
 --Design      : zynq_design_wrapper
 --Purpose     : IP block netlist
