e0eddef3df9885093c673249c4d9d2b4d01f1c90
FlorianMuttenthaler <flo.muttenthaler@gmail.com>
2019-01-13 14:41:07 +0100

    added blue filter implementation

---
+++ ImageProcessing/blue_filter/source/blue_filter_logic.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+--use ieee.std_logic_arith.all;
+--use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity blue_filter_logic is
+  generic(
+    -- Width of S_AXI data bus
+    C_S_AXI_DATA_WIDTH	: integer	:= 32
+  );
+  port(
+    clk 	: in std_logic;
+    regin   	: in std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+    regout   	: out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0)
+  );
+end black_white_filter_logic;
+
+architecture IMP of black_white_filter_logic is
+ 
+begin
+  process (clk)
+  begin
+    if clk'event and clk = '1' then
+      regout <= regin(7 downto 0);
+    end if;
+  end process;
+end IMP;
