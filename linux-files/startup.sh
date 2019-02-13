c0183e757d037034f0919157a42154fc34975ac4
FlorianMuttenthaler <flo.muttenthaler@gmail.com>
2019-02-13 14:24:25 +0100

    modified image_filter device driver plus related linux kernel files for handling additional device driver

--- linux-files/startup.sh
+++ linux-files/startup.sh
@@ -2,6 +2,8 @@ echo "++ Loading myled Module"
 insmod /mnt/sd/myled.ko
 echo "++ Loading blake2b Module"
 insmod /mnt/sd/blake2b.ko
+echo "++ Loading image_filter Module"
+insmod /mnt/sd/image_filter.ko
 
 echo "++ Preparing for Android" 
 mkdir /mnt/root 
