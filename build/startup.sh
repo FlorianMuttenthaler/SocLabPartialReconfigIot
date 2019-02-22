7a1fb603dbe03cff5485f1bde7421c2cd0b5cbb8
Benedikt Tutzer <e1225461@student.tuwien.ac.at>
2019-02-22 14:22:31 +0100

    Binary update

---
+++ build/startup.sh
@@ -0,0 +1,6 @@
+echo "++ Loading myled Module"
+insmod /mnt/sd/myled.ko
+echo "++ Loading blake2b Module"
+insmod /mnt/sd/blake2b.ko
+echo "++ Loading image_filter Module"
+insmod /mnt/sd/image_filter.ko
